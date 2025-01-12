88fe97089e16d9426925209490da8f098460ecbe
Daniel Tiana <daniieltiana@gmail.com>
2025-01-12 21:22:25 +0100

    Update recommended

--- Views/Home/Banniere/Banner_home.php
+++ Views/Home/Banniere/Banner_home.php
@@ -16,7 +16,7 @@
                     Voir les derniers nouvelles dans l'univers du gaming.
                     Le top de jeux modiales, et les animations
                 </p>
-                <a href="articles?type=film " class="stretched-link btn btn-dark">Voir tout</a>
+                <a href="articles?type=film " class="stretched-link btn btn-dark">Voir details</a>
             </div>
             <div class="col-auto d-none d-lg-block shadow">
                 <div class="feature-img">
@@ -29,16 +29,16 @@
         <div
             class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
             <div class="col p-4 d-flex flex-column position-static">
-                <!-- <strong class="d-inline-block mb-2 text-success">Design</strong> -->
-                <h3 class="mb-0">L'univers du movies</h3>
-                <p class="mb-auto">
-                    Vous cherchez les derniers sorties? vous êtes au bonne endroit ici. Survolez vous
+                <h3 class="mb-0 text-success">Le gaming</h3>
+                <p class="card-text mb-auto">
+                    Voir les derniers nouvelles dans l'univers du gaming.
+                    Le top de jeux modiales, et les animations
                 </p>
-                <a href="articles" class="stretched-link btn btn-dark">Voir tout</a>
+                <a href="articles?type=film " class="stretched-link btn btn-dark">Voir details</a>
             </div>
-            <div class="col-auto d-none d-lg-block">
+            <div class="col-auto d-none d-lg-block shadow">
                 <div class="feature-img">
-                    <video src="Assets/videos/lives.mp4" autoplay class="bd-placeholder-img"></video>
+                    <img src="Assets/images/slide/312123.jpg" class="bd-placeholder-img" alt="" width="200" height="250">
                 </div>
             </div>
         </div>
